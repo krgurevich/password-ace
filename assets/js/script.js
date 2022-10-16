060b83333699e763d497ca3ab844c2730f05847c
krgurevich <kananyan@optonline.net>
2022-10-16 10:43:48 -0400

    updated js code to fix the length and isNan function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declared Variables
 const length = "";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "!@#$%^&*_-+=";
 
-
+// Password Length Validation: Returns true if length is valid and the input is a number; returns false, if does not meet criteria
 function checkLength(length) {
-  if (length <= 8 || length >= 128) {
-    window.alert("Invalid Entry.  Please choose password length between 8 and 128")
-    generatePassword();
+  if (length < 8 || length > 128) {
+    return false;
+  } else if (isNaN(length)) {
+    return false;
   }
-  return length;
+  return true;
 }
 
-
+// Generates Password Function: Prompts the user for length, numbers, special character, uppercase, and lowercase
 function generatePassword() {
   let enteredLength = 0;
-  enteredLength = window.prompt("Please choose the length of the password");
-  console.log(enteredLength);
-  // const passwordLength = checkLength(enteredLength);
-  if (enteredLength <= 8 || enteredLength >= 128) {
-    window.alert("Invalid Entry.  Please choose password length between 8 and 128")
-    return;
+  enteredLength = window.prompt("Please choose password length between 8 and 128");
+  // This loop will continue checking until returned true
+  while (checkLength(enteredLength) === false) {
+    enteredLength = window.prompt("Invalid Entry!  Please choose password length between 8 and 128")
   }
-
   const numberResult = window.confirm("Would you like to include numbers?");
   const symbolResult = window.confirm("Would you like to include special characters?");
   const upperResult = window.confirm("Would you like to include uppercase letters?");
@@ -34,6 +33,8 @@ function generatePassword() {
 
   let arrayResult = [];
   let userResult = [];
+
+  // Once user's input is collected, the data is then split into an array of substrings and is merged into a new array
   if (numberResult) {
     arrayResult = arrayResult.concat(number.split(""))
   }
@@ -49,11 +50,12 @@ function generatePassword() {
   if (lowerResult) {
     arrayResult = arrayResult.concat(lowerCase.split(""))
   }
+  // Uses Math.random function to return a random password
   console.log(enteredLength);
   for (let i = 0; i < enteredLength; i++) {
     userResult.push(arrayResult[Math.floor(Math.random() * arrayResult.length)]);
   }
-
+  // Creates and returns a new string by concatenating of the elements in the array
   return userResult.join("");
 }
 
